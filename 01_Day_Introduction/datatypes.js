25c38c609e9a168bad0cbd2a00130f6f5230849c
iwegbujeddy <ijeddy5.0@gmail.com>
2022-12-05 00:58:22 +0100

    Added datatypes.js

---
+++ 01_Day_Introduction/datatypes.js
@@ -0,0 +1,9 @@
+let stringValue = "";
+let numberValue = 1;
+let objectValue = {};
+let arrayValue = [];
+
+typeof(stringValue);//string
+typeof(numberValue);//number
+typeof(objectValue);//object
+typeof(arrayValue);//array
