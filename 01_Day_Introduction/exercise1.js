0edabefeb6b299dbc87399cdcea0a438774bdebe
iwegbujeddy <ijeddy5.0@gmail.com>
2022-12-05 01:08:20 +0100

    Added to exercise1.js

--- 01_Day_Introduction/exercise1.js
+++ 01_Day_Introduction/exercise1.js
@@ -3,3 +3,21 @@
 /*3.  comments can make code readable, 
       easy to reuse and informative */
 
+ //6. let a,b,c,d;
+
+/* 7. let a = 0;
+      let b = 1;
+      let c = "hello";
+      let d = null;   
+*/
+/*8.*/ var firstName = "Iwegbu";
+       var lastName = "Jeddy";
+       var  maritalStatus = "single";
+       var country = "Nigeria";
+       var age = 18;
+/*9*/   var firstName = "Iwegbu"; var lastName = "Jeddy"; var maritalStatus = "single"; var country = "Nigeria"; var age = 18;
+/*10*/ var myAge = 25;
+       var yourAge = 29;
+       console.log("I am", myAge, "old.");
+       console.log("You are", yourAge, "old.");
+       
